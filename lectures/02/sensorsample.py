7f3c5f02a44731f7be058750a3b77f08da398378
root <root@bds-g05-n0.uvm.sdu.dk>
2023-10-05 10:28:24 +0200

    Cool commit bruh

--- lectures/02/sensorsample.py
+++ lectures/02/sensorsample.py
@@ -9,27 +9,27 @@ lastTime = time.time()
 hdfs_client = get_hdfs_client()
 
 def saveSample(sensorID, samplePayload):
-    createdAt = time.time()
+	createdAt = time.time()
 
-    payload: {
-        "sensor_id": sensorID,
-        "value": payload,
-        "unit": "MW",
-        "temporal_aspect": "real_time"
-    }
+	payload: {
+		"sensor_id": sensorID,
+		"value": payload,
+		"unit": "MW",
+		"temporal_aspect": "real_time"
+	}
 
-    sample = {
-        "payload": payload,
-        "correlation_id": str(uuid.uuid4()),
-        "created_at": createdAt,
-        "schema_version": 1
-    }
+	sample = {
+		"payload": payload,
+		"correlation_id": str(uuid.uuid4()),
+		"created_at": createdAt,
+		"schema_version": 1
+	}
 
-    sampleJSON = json.dumps(sample)
+	sampleJSON = json.dumps(sample)
 
-    timeDate = time.gmtime(createdAt)
+	timeDate = time.gmtime(createdAt)
 
-    hdfs_client.write("/data/raw/sensor_id={sample.payload}/temporal_aspect={sample.payload.temporal_aspect}/year={timeDate.tm_year}/month={timeDate.tm_month}/day={timeDate.tm_wday}/{sample.correlation_id}.json", sampleJSON, encoding="utf-8", overwrite=False)
+	hdfs_client.write("/data/raw/sensor_id={sample.payload}/temporal_aspect={sample.payload.temporal_aspect}/year={timeDate.tm_year}/month={timeDate.tm_month}/day={timeDate.tm_wday}/{sample.correlation_id}.json", sampleJSON, encoding="utf-8", overwrite=False)
 
 def fetchSample():
 	sensorID = random.randInt(1, 7)
@@ -38,6 +38,6 @@ def fetchSample():
 
 if __name__ == "__main__": 
 	while true:
-        if(time.time() - lastTime >= 2): # create sample every 2 seconds
-            fetchSample()
+		if(time.time() - lastTime >= 2):
+			fetchSample()
 
