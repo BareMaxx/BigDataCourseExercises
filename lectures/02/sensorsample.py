ef73c1bdc24a1ef8beae572d6f45c51549b9008a
root <root@bds-g05-n0.uvm.sdu.dk>
2023-10-05 10:59:03 +0200

    Now I am actually mad

--- lectures/02/sensorsample.py
+++ lectures/02/sensorsample.py
@@ -13,7 +13,7 @@ def saveSample(sensorID, samplePayload):
 
 	payload = {
 		"sensor_id": sensorID,
-		"value": payload,
+		"value": samplePayLoad,
 		"unit": "MW",
 		"temporal_aspect": "real_time"
 	}
